cd3f72b42fb33abcd71609cff41ba0ffae3edc36
Tony Blard <tonyblard55@gmail.com>
2023-03-21 15:18:21 +0100

    Create Appliation pixijs

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,16 @@
 console.log("Hello from the JavaScript console!")
-let app = new PIXI.Application({ width: 640, height: 360 });
+const app = new PIXI.Application();
+
+const resize = () => {
+    /* Resizing the canvas to the size of the window. */
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener("resize", () => {
+    /* Resizing the canvas to the size of the window. */
+    resize();
+})
+/* Setting the position of the canvas to absolute. */
+app.renderer.view.style.position = 'absolute';
+/* Adding the canvas to the body of the HTML document. */
 document.body.appendChild(app.view);
+resize();
