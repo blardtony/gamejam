665bbbc1faa4dc4010285eea424c0bd02ff2fca0
karimsalhi <karimsalhi.contact@gmail.com>
2023-03-22 12:47:31 +0100

    cat starts when we click

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -9,13 +9,6 @@ window.addEventListener("resize", () => {
 })
 game.resize();
 
-const cat = new Cat(game.width / 1, game.height / 2, 0.1666);
-const anim = await cat.getAnimationSprite();
-anim.play();
-
-// add it to the stage to render
-game.app.stage.addChild(anim);
-
 const gameStart = document.querySelector("#game-start");
 console.log(gameStart);
 
@@ -25,35 +18,45 @@ gameStart.addEventListener("click", () => {
     setTimeout(() => {
         gameOver.style.display = "block";
     }   , 10000);
+    start()
 });
 
 
 // After 10 seconds display the game over screen
+async function start() {
+    const cat = new Cat(game.width / 1, game.height / 2, 0.1666);
+    const anim = await cat.getAnimationSprite();
+    anim.play();
 
-// Make the anim interactive
-anim.interactive = true;
+    // add it to the stage to render
+    game.app.stage.addChild(anim);
 
-requestAnimationFrame(update);
+    // Make the anim interactive
+    anim.interactive = true;
 
-function update() {
-
-    if (anim.x < game.width / 2) {
-        anim.stop();
-        return
-    }
-    anim.x -= 3;
     requestAnimationFrame(update);
 
-    console.log("test")
-}
+    function update() {
+
+        if (anim.x < game.width / 2) {
+            anim.stop();
+            return
+        }
+        anim.x -= 3;
+        requestAnimationFrame(update);
+
+        console.log("test")
+    }
 
 
-// Set interactions on our anim 
-anim
-    .on('mousedown', onButtonDown)
-    .on('touchstart', onButtonDown)
-    
-function onButtonDown() {
-    anim.x = game.width / Math.floor(Math.random() * 10);
-    anim.y = game.height / Math.floor(Math.random() * 10);
+    // Set interactions on our anim 
+    anim
+        .on('mousedown', onButtonDown)
+        .on('touchstart', onButtonDown)
+        
+    function onButtonDown() {
+        anim.x = game.width / Math.floor(Math.random() * 10);
+        anim.y = game.height / Math.floor(Math.random() * 10);
+    }
 }
+
