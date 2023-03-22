470509692c4aeace909b2da4a984b7085dfbe594
Tony Blard <tonyblard55@gmail.com>
2023-03-22 11:35:55 +0100

    Game start and end menu Done

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -14,3 +14,17 @@ const anim = await cat.getAnimationSprite();
 anim.play();
 // add it to the stage to render
 game.app.stage.addChild(anim);
+
+const gameStart = document.querySelector("#game-start");
+console.log(gameStart);
+
+const gameOver = document.querySelector("#game-over");
+gameStart.addEventListener("click", () => {
+    gameStart.style.display = "none";
+    setTimeout(() => {
+        gameOver.style.display = "block";
+    }   , 10000);
+});
+
+
+// After 10 seconds display the game over screen
