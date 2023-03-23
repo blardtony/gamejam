2aaee787998463ca85e35ff3bf526910101e01d4
Tony Blard <tonyblard55@gmail.com>
2023-03-23 15:47:20 +0100

    Sprite animation chat done

--- assets/scripts/Cat.js
+++ assets/scripts/Cat.js
@@ -3,6 +3,14 @@ import { VanillaScoop, ChocolateScoop } from "./Scoop.js";
 import { Cone, Cup } from "./Container.js";
 import { ChocolateChips, Sprinkles } from "./Topping.js";
 
+/**
+ * Cat class
+ * @param {number} x - x position of the cat
+ * @param {number} y - y position of the cat
+ * @param {number} animationSpeed - speed of the cat animation
+ * @param {IceCream} iceCream - the ice cream the cat has
+ * @param {IceCream} iceCreamNeeded - the ice cream the cat needs
+ */
 export class Cat {
     constructor(x, y, animationSpeed) {
         this.x = x;
@@ -23,9 +31,9 @@ export class Cat {
         // const loader = PIXI.Loader.shared;
         const sheet = await PIXI.Assets.load('assets/img/pos.json');
         // console.log(sheet.data.meta.image);
-        const cats = ['assets/img/pos.png', 'assets/img/garfields.png', 'assets/img/pos.png'];
+        const cats = ['assets/img/pos.png', 'assets/img/garfields.png', 'assets/img/chats.png'];
         // const texture = await PIXI.Texture.from(cats[2]);
-        const texture = await PIXI.Texture.from(cats[Math.floor(Math.random() * 1000) % 3]);
+        const texture = await PIXI.Texture.from(cats[Math.floor(Math.random() *3)]);
         // console.log(texture);
         const spritesheet = new PIXI.Spritesheet(
             texture,
