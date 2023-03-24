22730b70b29ed07b7eef56062d3a946a21c1f7a4
karimsalhi <karimsalhi.contact@gmail.com>
2023-03-24 12:12:32 +0100

    Front - Assets positions

--- assets/scripts/Bucket.js
+++ assets/scripts/Bucket.js
@@ -1,9 +1,9 @@
 export class Bucket {
-    constructor(img, x, y, classType) {
+    constructor(img, x, y, classType, scale) {
         this.sprite = PIXI.Sprite.from(img);
         this.sprite.x = x;
         this.sprite.y = y;
-        this.sprite.scale.set(2);
+        this.sprite.scale.set(scale);
         this.sprite.interactive = true;
         this.sprite.buttonMode = true;
         this.classType = classType;
